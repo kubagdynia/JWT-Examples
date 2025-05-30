2792ad4a126b9ea48416225356547b4b9e309980
kubagdynia <kubagdynia@gmail.com>
2025-05-30 21:19:10 +0200

    fix tests

--- JwtExamples.Api.Tests/UnitTest1.cs
+++ JwtExamples.Api.Tests/UnitTest1.cs
@@ -98,8 +98,29 @@ public class Tests
         // Arrange
         using var client = _application.CreateClient();
         
+        List<Claim> claims =
+        [
+            new(ClaimTypes.Name, "Jan Kowalski"),
+            new(ClaimTypes.Email, "jan.kowalski@example.com"),
+            new(ClaimTypes.Role, "Admin")
+        ];
+        
+        var identity = new ClaimsIdentity(claims, "Bearer");
+        
+        var principal = new ClaimsPrincipal(identity);
+        
+        var encodedToken = CteateJwtToken(
+            securityKey: "secret-key-secret-key-secret-key-secret-key-secret-key-15321-15321-15321-15321",
+            issuer: "https://localhost:5001",
+            audience: "https://localhost:5001",
+            sub: null,
+            expires: DateTime.UtcNow.AddHours(24),
+            iat: DateTimeOffset.UtcNow,
+            claims);
+        
+        
         // Act
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiSmFuIEtvd2Fsc2tpIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoiamFuLmtvd2Fsc2tpQGV4YW1wbGUuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJleHAiOjE3MzUzODEzNTQsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo1MDAxIn0.Rm7KcWvgV46EJ9dfA1M8YSpQ_h63yFI52nDhp7bephw");
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", encodedToken);
         var response = await client.GetAsync("api/Test/hello-world3");
         
         // Assert
